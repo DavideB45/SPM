c0d5c38be8a6ea27cfb650c0052387233f99e007
DavideB <dadewww@gmail.com>
2025-03-09 19:08:15 +0100

    tests on spmcluster

---
+++ assignment1/slurm_run_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=db3_tests    # Job name
+#SBATCH --output=output.log     # Save output to a file
+#SBATCH --error=error.log       # Save error messages
+#SBATCH --time=00:04:00         # Set a time limit (4 minutes)
+#SBATCH --ntasks=1              # Number of tasks (processes)
+#SBATCH --cpus-per-task=1       # Number of CPU cores
+
+# Run make
+srun make all
+srun test.sh plain
+srun test.sh auto
+srun test.sh avx
