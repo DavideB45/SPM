e8d6ebe748837ff3dc7fa34bfbbce31c097abb0f
DavideB <dadewww@gmail.com>
2025-03-09 20:04:15 +0100

    more numbers

--- assignment1/slurm_run_tests.sh
+++ assignment1/slurm_run_tests.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=db3_tests    # Job name
 #SBATCH --output=output.log     # Save output to a file
 #SBATCH --error=error.log       # Save error messages
-#SBATCH --time=00:04:00         # Set a time limit (4 minutes)
+#SBATCH --time=00:20:00         # Set a time limit (20 minutes)
 #SBATCH --ntasks=1              # Number of tasks (processes)
 #SBATCH --cpus-per-task=1       # Number of CPU cores
 
