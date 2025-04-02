a1cc8d943cfcf5d233644c969afa845ae943fa7d
DavideB45 <dadewww@gmail.com>
2025-04-02 16:19:22 +0200

    you spin my head round round...

--- assignment2/lock_free_check.cpp
+++ assignment2/lock_free_check.cpp
@@ -13,22 +13,12 @@ using ull = unsigned long long;
 int main(){
     atomic<ull> a(0);
 
-    thread t1([&a](){
-        for (int i = 0; i < 1000000; i++){
-            a++;
-        }
-    });
-
-    thread t2([&a](){
-        for (int i = 0; i < 1000000; i++){
-            a++;
-        }
-    });
-
-    t1.join();
-    t2.join();
-
     cout << "Is lock free: " << (atomic_is_lock_free(&a) ? "true" : "false") << endl;
 
+    ull b = 0;
+    b = a.fetch_add(1);
+    cout << b << endl;
     cout << a << endl;
+
+
 }
--- assignment2/parallel_collatz.cpp
+++ assignment2/parallel_collatz.cpp
@@ -28,7 +28,8 @@ typedef struct {
 
 typedef struct {
 	vector<pair<ull, ull>> ranges;
-	vector<ull> max_steps;
+	vector<atomic<ull>> max_steps; // max number of steps for each range
+	vector<atomic<ull>> curr_steps; // current number of steps for each range
 } lock_free_data;
 
 /**
@@ -59,7 +60,7 @@ void update_max_steps(int range_index, ull steps, thread_data& collatz_data);
 /**
  * @brief Computes the Collatz sequence for the provided ranges using a dinamic schedule.
  */
-void dinamic_collatz(thread_data& collatz_data);
+void dynamic_collatz(thread_data& collatz_data);
 
 /**
  * @brief Computes the Collatz sequence for the provided ranges to be used by the spawned threads.
@@ -75,6 +76,30 @@ void static_collatz(thread_data& collatz_data);
  * @brief Computes the Collatz sequence for the provided ranges to be used by the spawned threads.
  */
 void static_compute(thread_data& collatz_data, int thread_id);
+
+/**
+ * @brief Updates the maximum number of steps for a given range index if the provided steps are greater.
+ * 
+ * This function is thread-safe and ensures that the maximum steps for a specific range index
+ * are updated only if the new steps value is greater than the current maximum. It uses atomic
+ * operations to synchronize access to the shared data structure.
+ * 
+ * @param range_index The index of the range for which the maximum steps are being updated.
+ * @param steps The number of steps to compare with the current maximum.
+ * @param collatz_data A reference to the lock_free_data structure containing the shared data,
+ *                     including the maximum steps array and the current steps array.
+ */
+void update_max_steps_atomic(int range_index, ull steps, lock_free_data& collatz_data);
+
+/**
+ * @brief Computes the Collatz sequence for the provided ranges using a lock-free approach.
+ */
+void lock_free_collatz(lock_free_data& collatz_data);
+
+/**
+ * @brief Computes the Collatz sequence for the provided ranges using a lock-free.
+ */
+void lock_free_compute(lock_free_data& collatz_data, int thread_id);
 
 int main(const int argc, const char *argv[]) {
 	if (argc < 2) {
@@ -101,6 +126,8 @@ int main(const int argc, const char *argv[]) {
 				/* chunk size */
 				i++;
 				task_size = stoi(argv[i]);
+			} else if(curr_arg[1] == 'f'){
+				lock_free = true;
 			} else {
 				cout << "Invalid argument: " << curr_arg << endl;
 				return 1;
@@ -118,6 +145,7 @@ int main(const int argc, const char *argv[]) {
 	printf("Number of threads: %d\n", num_threads);
 	printf("Task size: %d\n", task_size);
 
+	// allocate datastructure for the threads
 	thread_data ranges_data = {
 		ranges,                // ranges
 		vector<ull>(ranges.size(), 1), // max_steps
@@ -127,18 +155,34 @@ int main(const int argc, const char *argv[]) {
 		new mutex()             // curr_range_mutex
 	};
 
-	for (size_t i = 0; i < ranges.size(); i++) {
-		ranges_data.max_steps[i] = 1;
-	}
+	lock_free_data lock_free_data = {
+		ranges, 
+		vector<atomic<ull>>(ranges.size()),
+		vector<atomic<ull>>(ranges.size())
+	};
+
+	// for (size_t i = 0; i < ranges.size(); i++) {
+	// 	ranges_data.max_steps[i] = 1;
+	// }
 
 
 	if(dynamic_schedule){
-		dinamic_collatz(ranges_data);
+		if(lock_free){
+			lock_free_collatz(lock_free_data);
+			for(size_t i = 0; i < lock_free_data.ranges.size(); i++){
+				printf("Range [%llu, %llu]: max_steps = %llu\n", lock_free_data.ranges[i].first, lock_free_data.ranges[i].second, lock_free_data.max_steps[i].load());
+			}
+		} else {
+			dynamic_collatz(ranges_data);
+			for (size_t i = 0; i < ranges_data.ranges.size(); i++) {
+				printf("Range [%llu, %llu]: max_steps = %llu\n", ranges_data.ranges[i].first, ranges_data.ranges[i].second, ranges_data.max_steps[i]);
+			}
+		}
 	} else {
 		static_collatz(ranges_data);
-	}
-	for (size_t i = 0; i < ranges_data.ranges.size(); i++) {
-		printf("Range [%llu, %llu]: max_steps = %llu\n", ranges_data.ranges[i].first, ranges_data.ranges[i].second, ranges_data.max_steps[i]);
+		for (size_t i = 0; i < ranges_data.ranges.size(); i++) {
+			printf("Range [%llu, %llu]: max_steps = %llu\n", ranges_data.ranges[i].first, ranges_data.ranges[i].second, ranges_data.max_steps[i]);
+		}
 	}
 	return 0;
 }
@@ -152,7 +196,7 @@ ull collatz(ull n) {
 	return steps;
 }
 
-void dinamic_collatz(thread_data& collatz_data){
+void dynamic_collatz(thread_data& collatz_data){
 	TIMERSTART(total);
 	vector<thread> threads(num_threads);
 	for (int i = 0; i < num_threads; i++) {
@@ -251,4 +295,51 @@ void update_max_steps(int range_index, ull steps, thread_data& collatz_data){
 		collatz_data.max_steps[range_index] = steps;
 	}
 	collatz_data.max_steps_mutex->unlock();
+}
+
+void lock_free_collatz(lock_free_data& collatz_data){
+	TIMERSTART(total);
+	vector<thread> threads(num_threads);
+	for (int i = 0; i < num_threads; i++) {
+		threads[i] = thread(lock_free_compute, ref(collatz_data), i);
+	}
+	join_threads(threads);
+	TIMERSTOP(total);
+}
+
+void lock_free_compute(lock_free_data& collatz_data, int thread_id){
+	size_t chunk_index = 0;
+	ull begin_cr = collatz_data.ranges[chunk_index].first;; // begin of current range
+	ull end_cr = collatz_data.ranges[chunk_index].second;; // end of current range
+	ull curr_step; // step from which to start the chunk
+	ull curr_max = 0; // current max steps
+	ull end_task;
+	while(true){
+		
+		curr_step = begin_cr + collatz_data.curr_steps[chunk_index].fetch_add(task_size);
+
+		if(curr_step > end_cr){ // check if the range is finished
+			update_max_steps_atomic(chunk_index, curr_max, collatz_data);
+			chunk_index++;
+			if(chunk_index >= collatz_data.ranges.size()) break;				
+			curr_max = 0;
+			begin_cr = collatz_data.ranges[chunk_index].first;
+			end_cr = collatz_data.ranges[chunk_index].second;
+			continue;
+		}
+
+		end_task = min(curr_step + task_size, end_cr);
+		for(; curr_step < end_task; curr_step++){
+			curr_max = max(curr_max, collatz(curr_step));
+		}
+
+	}
+}
+
+void update_max_steps_atomic(int range_index, ull steps, lock_free_data& collatz_data){
+	ull curr_max = collatz_data.max_steps[range_index];
+	while (
+		curr_max < steps && 
+		!collatz_data.max_steps[range_index].compare_exchange_weak(curr_max, steps)
+	);
 }
