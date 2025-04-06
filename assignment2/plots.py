3ff71f83b4bf84a919eff6a64b60e4c8320b17f9
DavideB45 <dadewww@gmail.com>
2025-04-06 11:44:28 +0200

    lots of figures

--- assignment2/plots.py
+++ assignment2/plots.py
@@ -89,7 +89,8 @@ def plot_best_time(data: pd.DataFrame) -> None:
 	for i, problem in enumerate(["1", "2", "3"]):
 		ax[i].set_title(problem_names[i])
 		ax[i].set_xlabel("Number of Threads")
-		ax[i].set_ylabel("Time (s)")
+		#ax[i].set_ylabel("Time (s)")
+		ax[i].set_ylabel("Speedup")
 		#ax[i].set_yscale("log")  # Set y-axis to log scale
 		ax[i].set_xticks(range(6))
 		ax[i].set_xticklabels([1, 2, 4, 8, 16, 32])
@@ -103,5 +104,39 @@ def plot_best_time(data: pd.DataFrame) -> None:
 	plt.show()
 	
 		
+def plot_chunk_size(data: pd.DataFrame, dynamic:bool, lock_free:bool) -> None:
+	"""
+	Plot the chunk size for each problem.
+	"""
+	chunk_sizes = [1, 10, 50, 100, 1000]
+	problems = ["1", "2", "3"]
+	
+	fig, ax = plt.subplots(1, 3, figsize=(15, 5))
+	fig.suptitle("Chunk Size for Each Problem")
+	fig.tight_layout(pad=3.0)
+	for i, problem in enumerate(problems):
+		for chunk_size in chunk_sizes:
+			data_filtered = data[
+				(data["problem"] == int(problem)) & 
+				(data["chunk_size"] == chunk_size) &
+				(data["threads"].isin([1, 2, 4, 8, 16, 32])) &
+				(data["dynamic"] == int(dynamic)) &
+				(data["free"] == int(lock_free))
+			]
+			if data_filtered.empty:
+				continue
+			data_filtered = data_filtered.groupby("threads").agg({"time": "mean"}).reset_index()
+			ax[i].plot(data_filtered["threads"], data_filtered["time"], label=f"Chunk Size {chunk_size}", marker='x')
+		ax[i].set_title(f"Problem {problem}")
+		ax[i].set_xlabel("Number of Threads")
+		ax[i].set_ylabel("Time (s)")
+		ax[i].set_xticks([1, 2, 4, 8, 16, 32])
+		ax[i].set_xticklabels([1, 2, 4, 8, 16, 32])
+		ax[i].set_yscale("log")
+		ax[i].legend()
+		ax[i].grid()
+	fig.savefig(f"cs_{'dynamic' if dynamic else 'static'}_{'lock_free' if lock_free else ''}_log.png", dpi=300)
+	plt.show()
 
-plot_best_time(data)
+#plot_best_time(data)
+plot_chunk_size(data, dynamic=True, lock_free=True)
