4f3ad67706dbbcec8fc6dbdd4d2488a48d2e8faf
DavideB <dadewww@gmail.com>
2025-04-29 11:58:29 +0200

    removed affinity pers

--- assignment3/scripts/slurm_test_aff.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=Personal     # Job name
-#SBATCH --output=output.log     # Save output to a file
-#SBATCH --error=error.log       # Save error messages
-#SBATCH --time=00:30:00         # there are many tests
-#SBATCH --ntasks=1              # Number of tasks (processes)
-
-# Run the sequential tests
-srun ./scripts/test_affinity_personalized.sh
