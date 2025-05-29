375915e3d07366e5f36ace3eb2a67b472df4d86e
DavideB <dadewww@gmail.com>
2025-05-29 17:14:39 +0200

    weak scaling tested

--- assignment4/scripts/single_node.sh
+++ assignment4/scripts/single_node.sh
@@ -2,8 +2,9 @@
 #SBATCH --job-name=ff_tests     # Job name
 #SBATCH --output=output.log     # Save output to a file
 #SBATCH --error=error.log       # Save error messages
-#SBATCH --time=00:30:00         # there are many tests
+#SBATCH --time=00:05:00         # there are many tests
 #SBATCH --ntasks=1              # Number of tasks (processes)
+#SBATCH --priority=1            # Set maximum priority
 
 # Run make
 srun make all -j
@@ -12,7 +13,7 @@ srun make all -j
 # srun ./scripts/test_sequential.sh
 
 # Run the parallel tests
-srun ./scripts/test_naive_ff.sh
+# srun ./scripts/test_naive_ff.sh
 # srun ./scripts/test_working_emitter_ff.sh
 
 # weak scaling tests
