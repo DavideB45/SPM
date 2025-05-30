3dfc5be975ffd5fed23657e883dac66a2126342c
DavideB45 <dadewww@gmail.com>
2025-05-30 19:38:20 +0200

    nothing much

--- assignment4/s_mpi.cpp
+++ assignment4/s_mpi.cpp
@@ -1,21 +1,49 @@
 #include <mpi.h>
 #include <iostream>
+#include <unistd.h>
+// My stuff
+#include <structures.hpp>
+#include <parse_argv.hpp>
+#include <config.hpp>
+#include <hpc_helpers.hpp>
+
+Record* records = nullptr;
 
 int main(int argc, char* argv[]) {
-    int provided;
-    MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+	int error = parse_argv(argc, argv);
+	if (error != 0) {
+		fprintf(stderr, "Error parsing arguments at index %d\n", error);
+		return error;
+	}
 
-    if (provided < MPI_THREAD_MULTIPLE) {
-        std::cerr << "MPI does not support required threading level." << std::endl;
-        MPI_Abort(MPI_COMM_WORLD, 1);
-    }
+	//TODO: substitute with a read from a file to speedup testing
+	//records = random_generate(ARRAY_SIZE);
 
-    int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+	int provided;
+	TIMERSTART(mpiinit)
+	MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+	TIMERSTOP(mpiinit);
+	
+	if (provided < MPI_THREAD_MULTIPLE) {
+		std::cerr << "MPI does not support required threading level." << std::endl;
+		MPI_Abort(MPI_COMM_WORLD, 1);
+	}
 
-    std::cout << "Hello world from rank " << rank << " out of " << size << " processes!" << std::endl;
+	TIMERSTART(mpiworld)
+	int rank, size;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	if (rank == 0) {
+		TIMERSTOP(mpiworld);
+	}
 
-    MPI_Finalize();
-    return 0;
+	if (rank == 1){
+		sleep(1);
+	}
+	
+
+	std::cout << "Hello world from rank " << rank << " out of " << size << " processes!" << std::endl;
+	
+	MPI_Finalize();
+	return 0;
 }
