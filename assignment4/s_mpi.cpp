e1459a02d0664b5ed026e67a5355fb0a3edb4975
DavideB45 <dadewww@gmail.com>
2025-05-30 14:53:21 +0200

    mpi hello, some notebook

--- assignment4/s_mpi.cpp
+++ assignment4/s_mpi.cpp
@@ -1,3 +1,21 @@
-int main(){
-    return 0; // Placeholder for main function
+#include <mpi.h>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    int provided;
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+
+    if (provided < MPI_THREAD_MULTIPLE) {
+        std::cerr << "MPI does not support required threading level." << std::endl;
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    std::cout << "Hello world from rank " << rank << " out of " << size << " processes!" << std::endl;
+
+    MPI_Finalize();
+    return 0;
 }
