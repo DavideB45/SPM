1755bdc0c597613da4717d0f0533732493a331cd
DavideB <dadewww@gmail.com>
2025-06-01 14:02:27 +0200

    few results

--- assignment4/scripts/test_mpi_seq.sh
+++ assignment4/scripts/test_mpi_seq.sh
@@ -2,18 +2,19 @@
 # test_mpi_seq.sh
 # Launches the program on 1, 2, 4, and 8 nodes, one after the other, and logs results.
 
-srun -n 1 make all
+#srun -n 1 make all
 
 PAYLOADS=(1)
-ARRAY_SIZE=(1000000 10000000 50000000 100000000)
-MPI_BUFF_SIZE=(128 10000 25000 100000)
+ARRAY_SIZE=(10000000 50000000 100000000)
+MPI_BUFF_SIZE=(1000000 3000000 9999999999999)
+NODES_T=(2 4 8)
 OUTPUT_FILE="results/multinode_seq.csv"  # Log file
 
 # Initialize the CSV file
-echo "nodes,trial,size,buff_size,time" > $OUTPUT_FILE
+#echo "nodes,trial,p,size,buff_size,time" > $OUTPUT_FILE
 
 # Loop through node counts
-for N in 2 4 8; do
+for N in ${NODES_T[@]}; do
     echo "Running on $N node(s)"
     # Loop through trials
     for i in {1..1}; do
@@ -26,7 +27,7 @@ for N in 2 4 8; do
                      --ntasks-per-node=1 \
                      --time=00:04:00 \
                      --mpi=pmix \
-                     ./mpi.a -r "${PAYLOADS[0]}" -s "$size" -b "$buff_size" | grep 'sort_records' | grep -o '[0-9.]\+' >> $OUTPUT_FILE
+                     ./s_mpi.a -r "${PAYLOADS[0]}" -s "$size" -b "$buff_size" | grep 'sort_records' | grep -o '[0-9.]\+' >> $OUTPUT_FILE
             done
         done
     done
