6c54368ec03fae32b2b003c09852597e06c0af09
DavideB45 <dadewww@gmail.com>
2025-06-02 14:19:17 +0200

    prepared tests

--- assignment4/scripts/test_mpi_ff_db.sh
+++ assignment4/scripts/test_mpi_ff_db.sh
@@ -2,65 +2,37 @@
 # test_mpi_seq.sh
 # Launches the program on 1, 2, 4, and 8 nodes, one after the other, and logs results.
 
-srun -n 1 --time=00:02:00 make all
+srun -n 1 --time=00:02:00 make s_mpi_ff_db
 
-PAYLOADS=(1)
-ARRAY_SIZE=(50000000 100000000)
-MPI_BUFF_SIZE=(9999999999999)
+# task size will be the default 10000
+ARRAY_SIZE=(10000000 50000000 100000000)
+MPI_BUFF_SIZE=(1000000 2000000 4000000 10000000 )
 NODES_T=(2 4 8)
 THREAD_COUNTS=(7 15)
-OUTPUT_FILE="results/multinode_ff.csv"  # Log file
+OUTPUT_FILE="results/multinode_ff_db.csv"  # Log file
 
 # Initialize the CSV file
-echo "nodes,trial,payload,size,buff_size,thread,time" > $OUTPUT_FILE
+echo "nodes,size,buff_size,thread,time" > $OUTPUT_FILE
 
 # Loop through node counts
 for N in ${NODES_T[@]}; do
     echo "Running on $N node(s)"
-    # Loop through trials
-    for i in {1..1}; do
-        # Loop through each array size
-        for size in "${ARRAY_SIZE[@]}"; do
-            # Loop through each buffer size
-            for buff_size in "${MPI_BUFF_SIZE[@]}"; do
-                # Loop through thread counts
-                for threads in "${THREAD_COUNTS[@]}"; do
-                    echo -n "$N,$i,${PAYLOADS[0]},$size,$buff_size,$threads," >> $OUTPUT_FILE
-                    srun --nodes=$N \
-                         --ntasks-per-node=1 \
-                         --time=00:04:00 \
-                         --mpi=pmix \
-                         ./s_mpi_ff.a -r "${PAYLOADS[0]}" -s "$size" -b "$buff_size" -t "$threads" | grep 'sort_records' | grep -o '[0-9.]\+' >> $OUTPUT_FILE
-                done
-            done
-        done
-    done
-done
-
-# test with 2 processes per node
-OUTPUT_FILE="results/multinode_ff_2.csv"  # Log file
-
-# Initialize the CSV file
-echo "nodes,trial,payload,size,buff_size,time" > $OUTPUT_FILE
-
-# Loop through node counts
-for N in ${NODES_T[@]}; do
-    echo "Running on $N node(s)"
-    # Loop through trials
-    for i in {1..1}; do
-        # Loop through each array size
-        for size in "${ARRAY_SIZE[@]}"; do
-            # Loop through each buffer size
-            for buff_size in "${MPI_BUFF_SIZE[@]}"; do
-                # Loop through thread counts
-                for threads in "${THREAD_COUNTS[@]}"; do
-                    echo -n "$N,$i,${PAYLOADS[0]},$size,$buff_size,$threads," >> $OUTPUT_FILE
-                    srun --nodes=$N \
-                         --ntasks-per-node=2 \
-                         --time=00:04:00 \
-                         --mpi=pmix \
-                         ./s_mpi_ff.a -r "${PAYLOADS[0]}" -s "$size" -b "$buff_size" -t "$threads" | grep 'sort_records' | grep -o '[0-9.]\+' >> $OUTPUT_FILE
-                done
+    # Loop through each array size
+    for size in "${ARRAY_SIZE[@]}"; do
+        # Loop through each buffer size
+        for buff_size in "${MPI_BUFF_SIZE[@]}"; do
+            # Loop through thread counts
+            for threads in "${THREAD_COUNTS[@]}"; do
+                if [ $((buff_size * N)) -ge $size ]; then
+                    echo "Skipping buff_size=$buff_size, nodes=$N, size=$size as double buffering is not useful"
+                    continue
+                fi
+                echo -n "$N,$size,$buff_size,$threads," >> $OUTPUT_FILE
+                srun --nodes=$N \
+                    --ntasks-per-node=1 \
+                    --time=00:03:00 \
+                    --mpi=pmix \
+                    ./s_mpi_ff_db.a -r "1" -s "$size" -b "$buff_size" -t "$threads" | grep 'sort_records' | grep -o '[0-9.]\+' >> $OUTPUT_FILE
             done
         done
     done
