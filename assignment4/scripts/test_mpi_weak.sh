a1ca46a4eb20218e02a85f12f727afeb82fa02a0
DavideB <dadewww@gmail.com>
2025-06-04 11:09:41 +0200

    weak for mpi completed

--- assignment4/scripts/test_mpi_weak.sh
+++ assignment4/scripts/test_mpi_weak.sh
@@ -11,21 +11,21 @@ SIZES=(13993959 26917759 51848806) # 99999999 was already done
 COMPUTERS=(1 2 4) # 8 was already done
 
 # # create a csv file for the sequential results
-OUTPUT_FILE="results/multinode_ff_weak.csv"  # Log file
-# #initialize the csv file
-echo "nodes,size,workers,time" > $OUTPUT_FILE
+# OUTPUT_FILE="results/multinode_ff_weak.csv"  # Log file
+# # #initialize the csv file
+# echo "nodes,size,workers,time" > $OUTPUT_FILE
 
-for i in "${!COMPUTERS[@]}"; do
-    nodes=${COMPUTERS[$i]}
-    size=${SIZES[$i]}
-    # run the sequential program
-    echo -n "$nodes,$size,$workers," >> $OUTPUT_FILE
-    srun --nodes=$nodes \
-         --ntasks-per-node=1 \
-         --time=00:02:00 \
-         --mpi=pmix \
-         ./s_mpi_ff.a -r "$payload" -s "$size" -t "$workers" | grep 'sort_records' | grep -o '[0-9.]\+' >> $OUTPUT_FILE
-done
+# for i in "${!COMPUTERS[@]}"; do
+#     nodes=${COMPUTERS[$i]}
+#     size=${SIZES[$i]}
+#     # run the sequential program
+#     echo -n "$nodes,$size,$workers," >> $OUTPUT_FILE
+#     srun --nodes=$nodes \
+#          --ntasks-per-node=1 \
+#          --time=00:02:00 \
+#          --mpi=pmix \
+#          ./s_mpi_ff.a -r "$payload" -s "$size" -t "$workers" | grep 'sort_records' | grep -o '[0-9.]\+' >> $OUTPUT_FILE
+# done
 
 OUTPUT_FILE="results/multinode_seq_weak.csv"  # Log file
 # #initialize the csv file
@@ -39,5 +39,5 @@ for i in "${!COMPUTERS[@]}"; do
          --ntasks-per-node=1 \
          --time=00:02:00 \
          --mpi=pmix \
-         ./s_mpi_seq.a -r "$payload" -s "$size" | grep 'sort_records' | grep -o '[0-9.]\+' >> $OUTPUT_FILE
+         ./s_mpi.a -r "$payload" -s "$size" | grep 'sort_records' | grep -o '[0-9.]\+' >> $OUTPUT_FILE
 done
